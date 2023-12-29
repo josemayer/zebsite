364adc2b9aee640b23f891afed4801b49a83e5ed
José Lucas Silva Mayer <josemayer@usp.br>
2023-12-29 11:38:34 +0000

    Update main www page
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- httpd/htdocs/www/script.js
+++ httpd/htdocs/www/script.js
@@ -1,75 +1,108 @@
-"use strict";
-class Box extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `<div class="front"></div>
-      <div class="back"></div>
-      <div class="right"></div>
-      <div class="left"></div>
-      <div class="bottom"></div>
-      <div class="top"></div>`;
-    }
+import { Those3DTexts } from "https://cdn.skypack.dev/that-3d-text-library";
+import { gsap } from "https://cdn.skypack.dev/gsap";
+import { MotionPathPlugin } from "https://cdn.skypack.dev/gsap/MotionPathPlugin";
+
+gsap.registerPlugin(MotionPathPlugin);
+
+const letters = ['😀', '😆', '🥹', '🥰', '😎', '🤔', '🥸', '😡', '😱', '👍'];
+
+new Those3DTexts();
+
+const STYLE_COUNT = 16;
+const FONT_COUNT = 1;
+const BOUNCE_DURATION = 1.3;
+const BOUNCE_HEIGHT = -90;
+const BOUNCE_WOBBLE = 0.75; // lower = more wobble
+const FLOOR_PARTICLE_COUNT = 1;
+const AIR_PARTICLE_COUNT =4;
+
+const LETTER = document.querySelector('h1')
+const BODY = document.body
+
+let count = 0;
+
+BODY.classList.add('style-0');
+LETTER.classList.add('font-0');
+
+const setNextStyle = () => {
+
+  BODY.classList.remove(`style-${count % (STYLE_COUNT)}`);
+  LETTER.classList.remove(`font-${count % (FONT_COUNT)}`);
+  count++;
+  BODY.classList.add(`style-${count % (STYLE_COUNT)}`);
+  LETTER.classList.add(`font-${count % (FONT_COUNT)}`);
 }
-window.customElements.define("css-box", Box);
-class Person extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `<div class="person-top">
-      <div class="head-wrapper">
-        <css-box class="head"></css-box>
-      </div>
-      <div class="arm">
-        <css-box class="arm-top"></css-box>
-        <css-box class="arm-bottom"></css-box>
-      </div>
-      <css-box class="torso"></css-box>
-      <div class="arm arm-right">
-        <css-box class="arm-top"></css-box>
-        <css-box class="arm-bottom"></css-box>
-      </div>
-    </div>
-    <div class="legs">
-      <div class="leg leg-left">
-        <css-box class="leg-top"></css-box>
-        <css-box class="leg-bottom"></css-box>
-        <css-box class="leg-shoe"></css-box>
-      </div>
-      <div class="leg leg-right">
-        <css-box class="leg-top"></css-box>
-        <css-box class="leg-bottom"></css-box>
-        <css-box class="leg-shoe"></css-box>
-      </div>
-    </div>`;
-    }
+
+const particles = {
+  floor: [],
+  air: []
 }
-window.customElements.define("css-person", Person);
-const addMoreButton = document.querySelector(".add-more-button");
-const row = document.querySelector(".row");
-addMoreButton.addEventListener("click", () => {
-    if (document.querySelectorAll("css-person").length >= 30) {
-        return;
-    }
-    row.innerHTML += `<css-person></css-person>`;
-});
-// Add more people on safari
-const ua = navigator.userAgent.toLowerCase();
-if (ua.indexOf("safari") > -1) {
-    if (ua.indexOf("chrome") > -1) {
-        // Chrome
-    }
-    else {
-        row.innerHTML += `<css-person></css-person>
-    <css-person></css-person>
-    <css-person></css-person>
-    <css-person></css-person>
-    <css-person></css-person>
-    <css-person></css-person>`;
-    }
+
+const setupParticles = () => {
+  
+  const floor = document.querySelector('.floor');
+  const air = document.querySelector('.air');
+  
+  for(let i = 0; i < FLOOR_PARTICLE_COUNT; i++) {
+    let particle = document.createElement('div');
+    floor.appendChild(particle)
+    particles.floor.push(particle)
+  }
+  
+  for(let i = 0; i < AIR_PARTICLE_COUNT; i++) {
+    let particle = document.createElement('div');
+    air.appendChild(particle)
+    particles.air.push(particle)
+  }
 }
-// -------- For debug purposes -------- //
-// const wrapper = document.querySelector('.wrapper');
-// window.addEventListener('mousemove', (e) => {
-//   wrapper.style.transform = `rotateY(${
-//     (e.clientX / window.innerWidth) * 360
-//   }deg) rotateX(5deg)`;
-// });
+
+setupParticles()
+
+
+let letterCount = 0
+
+const nextLetter = () => {
+  letterCount ++;
+  const letterElements = LETTER.querySelectorAll('span') 
+  letterElements.forEach(l => l.innerText = letters[letterCount % letters.length])
+}
+
+const glitch = () => {
+  gsap.to('h1 span', {keyframes: [{opacity: 'random(0, 1)', xPercent: 'random(-10, 10)', opacity: 'random(0, 1)', yPercent: 'random(-10, 10)'}, {opacity: 'random(0, 1)', xPercent: 'random(-20, 20)', yPercent: 'random(-20, 20)'}, {opacity: 'random(0, 1)', xPercent: 'random(-10, 10)', yPercent: 'random(-10, 10)'}, {opacity: 1, xPercent: 0, yPercent: 0}], yoyo: true, ease: 'power4.inOut', duration: BOUNCE_DURATION * 0.5})
+}
+
+const bounce = () => {
+  const tl = gsap.timeline({defaults: { duration: BOUNCE_DURATION * 0.5}, onComplete: () => {
+    setNextStyle();  
+    nextLetter();
+    bounce();
+  }})
+  tl.set('.squish', {scaleY: BOUNCE_WOBBLE, scaleX: 1 + (1 - BOUNCE_WOBBLE)})
+  tl.to('.squish', {scaleY: 1, scaleX: 1, duration: BOUNCE_DURATION * 0.6, ease: 'elastic'}, 0)
+  tl.to(LETTER, {yPercent: BOUNCE_HEIGHT, ease: 'circ.out'}, 0)
+  tl.to('.shadow', {opacity: 0.2, ease: 'circ.out'}, 0)
+  tl.to(LETTER, {yPercent: 10, ease: 'circ.in'}, BOUNCE_DURATION * 0.5)
+  tl.to('.shadow', {opacity: 1, ease: 'circ.in'}, BOUNCE_DURATION * 0.5)
+  tl.to(LETTER, {rotationZ: "random(-150, 150)", rotationY: "random(-50, 50)", rotationX: "random(-50, 50)", duration: BOUNCE_DURATION, ease: 'none'}, 0)
+  // tl.to('.squish', {scaleY: BOUNCE_WOBBLE, duration: 0.2}, '-=0.2')
+  
+  
+  gsap.fromTo(particles.floor, {x: 0, y: 0,  scale: 1, opacity: 0.7}, {duration:  Math.min(BOUNCE_DURATION, 0.7 + Math.random() * 0.8), ease: 'power4.out', opacity: 0, scale: 2.5})
+  
+  particles.air.forEach((particle) => {
+    particle.setAttribute('class', `p${Math.floor(Math.random() * 4)}`)
+    gsap.fromTo(particle, {opacity: 1, x: 0, y: 0, z: 0, scale: 'random(0.3, 1)'}, { 
+      duration: Math.min(BOUNCE_DURATION, 0.3 + Math.random() * 0.3),
+      ease: 'linear',
+      opacity: 0,
+      rotate: 'random(-100, 100)',
+      x: 'random(-250, 250)',
+      y: 'random(-200, -100)',
+      z: 'random(-400, 400)',
+    })
+  })
+  
+  if(Math.random() > 0.95) glitch()
+}
+
+bounce();
