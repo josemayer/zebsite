209baba87451392430c7b37c2ce9589ef59c54a9
José Lucas Silva Mayer <josemayer@usp.br>
2024-02-01 23:39:28 -0400

    Increase container size to all breakpoints
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- web/lobo/src/App.js
+++ web/lobo/src/App.js
@@ -100,7 +100,7 @@ function App() {
           setCurrentScreen,
         }}
       >
-        <div className="flex flex-col justify-start p-4 w-[250px] sm:w-[300px] md:w-[400px] max-h-screen">
+        <div className="flex flex-col justify-start p-4 w-[375px] sm:w-[450px] md:w-[500px] max-h-screen">
           <Logo />
           <Error error={error} />
 
