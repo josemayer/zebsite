49dc0ee6c7a2e25333c5db7a8fedc8f46463389e
José Lucas Silva Mayer <josemayer@usp.br>
2024-02-14 11:44:42 -0400

    Add TailwindCSS
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

---
+++ web/www/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#6524BF",
+        green: "#29A655",
+        lightGreen: "#62F596",
+        yellow: "#F2BF27",
+        orange: "#F27D16",
+        red: "#F21616",
+      },
+    },
+  },
+  plugins: [],
+};
