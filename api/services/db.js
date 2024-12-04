f92cbc1e848d08996da890ce954cbfbebdf7530c
José Lucas Silva Mayer <josemayer@usp.br>
2024-12-04 19:33:26 -0300

    add close pool method
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- api/services/db.js
+++ api/services/db.js
@@ -1,20 +1,29 @@
-const { Pool } = require('pg');
-const config = require('../config');
+const { Pool } = require("pg");
+const config = require("../config");
 const pool = new Pool(config.db);
 
 /**
  * Query the database using the pool
- * @param {*} query 
- * @param {*} params 
- * 
+ * @param {*} query
+ * @param {*} params
+ *
  * @see https://node-postgres.com/features/pooling#single-query
  */
 async function query(query, params) {
-    const {rows, fields} = await pool.query(query, params);
+  const { rows, fields } = await pool.query(query, params);
 
-    return rows;
+  return rows;
+}
+
+/**
+ * Gracefully terminate the pool
+ */
+async function close() {
+  await pool.end();
+  console.log("PostgreSQL pool has ended");
 }
 
 module.exports = {
-  query
-}
+  query,
+  close,
+};
