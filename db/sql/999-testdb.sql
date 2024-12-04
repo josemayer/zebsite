91f070b289b1ce6fc29d9b4fdb34f2ac226334a7
José Lucas Silva Mayer <josemayer@usp.br>
2024-12-04 18:53:46 -0300

    modify to only create test db
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- db/sql/999-testdb.sql
+++ db/sql/999-testdb.sql
@@ -1,13 +1,4 @@
 -- ============== TEST SETUP ==============
 
--- Create a new role for the test user
-CREATE ROLE test_user WITH LOGIN PASSWORD 'test_pwd';
-
--- Create a new database for the test user
+-- Create a new database for tests
 CREATE DATABASE test WITH OWNER = postgres TEMPLATE = postgres;
-
--- Grant all privileges on the test database to the test user
-GRANT ALL PRIVILEGES ON DATABASE test TO test_user;
-
--- Only allow the test user to connect to the test database
-REVOKE CONNECT ON DATABASE postgres FROM test_user;
