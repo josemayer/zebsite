d8e610b9f4671046a17ff76732b2ce29fdc6ea66
José Lucas Silva Mayer <josemayer@usp.br>
2025-12-14 02:15:48 +0000

    fix: use new name of loading
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- web/mc/src/components/Loading.tsx
+++ web/mc/src/components/Loading.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { AiOutlineLoading } from "react-icons/ai";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Loading: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <AiOutlineLoading className="text-3xl animate-spin" />
+      <AiOutlineLoading3Quarters className="text-3xl animate-spin" />
     </div>
   );
 };
