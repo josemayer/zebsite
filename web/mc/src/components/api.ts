c18a8ea078ee16db2a8b60ed28d32c6037f8de73
José Lucas Silva Mayer <josemayer@usp.br>
2025-12-20 21:03:19 +0000

    fix: use api prod if dont exist env
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- web/mc/src/components/api.ts
+++ web/mc/src/components/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_ENDPOINT,
+  baseURL: process.env.REACT_APP_API_ENDPOINT || "https://api.josemayer.dev"
 });
 
 // Interceptor to automatically add the Auth Token from cookies to every request
